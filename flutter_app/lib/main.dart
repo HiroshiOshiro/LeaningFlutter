301cbdd6b62e15df4fdb56ef5df30992b2a5454f
hiroshi oshiro <ooshiro010@gmail.com>
2020-10-17 19:03:04 +0900

    3-1-3-4 StatefullWidget

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         title: 'Flutter App',
+//         theme: ThemeData(
+//           primarySwatch: Colors.blue,
+//         ),
+//         home: Scaffold(
+//           body: Center(
+//             child: Text(
+//               'Flutter Demo Home Page',
+//               style: Theme.of(context).textTheme.display1,
+//             ),
+//           ),
+//         ),
+//       );
+// }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
+
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
         home: Scaffold(
           body: Center(
-            child: Text(
-              'Flutter Demo Home Page',
-              style: Theme.of(context).textTheme.display1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('You have pushed the button this many times:'),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.display1,
+                ),
+              ],
             ),
           ),
         ),
