ed373df40b9c920cb63e0fcc1f04017b2687a297
hiroshi oshiro <ooshiro010@gmail.com>
2021-02-06 19:10:05 +0900

    added EventListBloc

---
+++ sample_application/sample_app_chapter_6/lib/blocs/event_list_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:meta/meta.dart';
+import 'package:sample_app_chapter_6/blocs/event_list_event.dart';
+import 'package:sample_app_chapter_6/blocs/event_list_repository.dart';
+import 'package:sample_app_chapter_6/blocs/event_list_state.dart';
+
+class EventListBloc extends Bloc<EventListEvent, EventListState> {
+  final EventListRepository _eventListRepository;
+
+  EventListBloc({@required EventListRepository eventListRepository})
+      : assert(eventListRepository != null),
+        _eventListRepository = eventListRepository;
+
+  @override
+  EventListState get initialState => EventListEmpty();
+
+  @override
+  Stream<EventListState> mapEventToState(EventListEvent event) async* {
+    if (event is EventListLoad) {
+      yield* _mapEventListLoadToState();
+    }
+  }
+
+  Stream<EventListState> _mapEventListLoadToState() async* {
+    yield EventListInProgress();
+    try {
+      yield EventListSuccess(eventList: _eventListRepository.fetch());
+    } catch (_) {
+      yield EventListFailure(error: _);
+    }
+  }
+}
