8b9b2eefb0f8af385ded4bbd5dda3920e1189af3
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-08-16 23:07:50 +0900

    refactoring. video 144

--- Udemy/expense_maneger/expense_manager/lib/widgets/transaction_list.dart
+++ Udemy/expense_maneger/expense_manager/lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:expense_manager/models/transaction.dart';
+import 'package:expense_manager/widgets/transaction_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -35,90 +35,9 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemCount: transactions.length,
               itemBuilder: (context, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: FittedBox(
-                            child: Text(
-                                '\$${transactions[index].amount.toStringAsFixed(2)}')),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.title,
-                    ),
-                    subtitle: Text(DateFormat.yMMMd('en_US')
-                        .add_jm()
-                        .format(transactions[index].date)),
-                    trailing: MediaQuery.of(context).size.width > 400
-                        ? TextButton.icon(
-                            label: Text("Delete"),
-                            style: TextButton.styleFrom(
-                                primary: Theme.of(context).errorColor),
-                            icon: Icon(Icons.delete),
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                          ),
-                  ),
-                );
-                // return Card(
-                //   child: Row(
-                //     children: <Widget>[
-                //       Container(
-                //         padding: EdgeInsets.all(10),
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         decoration: BoxDecoration(
-                //             border: Border.all(
-                //           color: Theme.of(context).primaryColor,
-                //           width: 2,
-                //         )),
-                //         child: Text(
-                //           '\$${transactions[index].amount.toStringAsFixed(2)}',
-                //           style: TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: <Widget>[
-                //           Text(
-                //             transactions[index].title,
-                //             style: Theme.of(context).textTheme.title,
-                //           ),
-                //           Text(
-                //             DateFormat.yMMMMd("en_US")
-                //                 .add_jm()
-                //                 .format(transactions[index].date),
-                //             style: TextStyle(
-                //               fontSize: 13,
-                //               color: Colors.grey,
-                //             ),
-                //           )
-                //         ],
-                //       )
-                //     ],
-                //   ),
-                // );
+                return TransactionItem(
+                    transaction: transactions[index],
+                    deleteTransaction: deleteTransaction);
               }),
     );
   }
