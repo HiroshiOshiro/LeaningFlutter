0bcfd3499662fd833b938a238141613331dfcc23
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-08-30 23:50:42 +0900

    add GridView, show dummy data (video 160)

--- Udemy/deli_meals/lib/categories_screen.dart
+++ Udemy/deli_meals/lib/categories_screen.dart
@@ -1,16 +1,32 @@
+import 'package:deli_meals/category_item.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'models/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeal"),
+        titleTextStyle: Theme.of(context).textTheme.headline6,
       ),
-      children: [],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((data) => CategoryItem(
+                  title: data.title,
+                  color: data.color,
+                ))
+            .toList(),
+      ),
     );
   }
 }
