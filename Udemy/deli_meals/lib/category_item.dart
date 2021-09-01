ad92d3106324acaa2d6deaba47f23bb8f56b7d96
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-09-01 22:55:36 +0900

    Navigation by named route (video 163)

--- Udemy/deli_meals/lib/category_item.dart
+++ Udemy/deli_meals/lib/category_item.dart
@@ -1,4 +1,3 @@
-import 'package:deli_meals/category_meals_screen.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
@@ -10,15 +9,12 @@ class CategoryItem extends StatelessWidget {
       : super(key: key);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(
-            categoryId: id,
-            categoryTitle: title,
-          );
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
