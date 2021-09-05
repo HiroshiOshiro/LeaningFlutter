b74b5d2370c458b193387d85d302453b7b2bd041
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-09-05 10:57:34 +0900

    use const for named route

--- Udemy/deli_meals/lib/category_meals_screen.dart
+++ Udemy/deli_meals/lib/category_meals_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/catergory-meals';
+
   // final String categoryId;
   // final String categoryTitle;
   //
