b7afa218c6afbdec8b1956797c97d71518b40059
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-09-18 10:47:51 +0900

    added MealDetailScreen

--- Udemy/deli_meals/lib/widgets/meal_item.dart
+++ Udemy/deli_meals/lib/widgets/meal_item.dart
@@ -1,7 +1,10 @@
 import 'package:deli_meals/models/meal.dart';
 import 'package:flutter/material.dart';
 
+import '../screens/meal_detail_screen.dart';
+
 class MealItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
   final int duration;
@@ -10,6 +13,7 @@ class MealItem extends StatelessWidget {
 
   const MealItem(
       {Key key,
+      @required this.id,
       @required this.title,
       @required this.imageUrl,
       @required this.duration,
@@ -49,11 +53,17 @@ class MealItem extends StatelessWidget {
     }
   }
 
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: id,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    void selectMeal() {}
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
