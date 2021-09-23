9736fcf0f70b25e653f190808e3196249770e86a
Hiroshi Oshiro <ooshiro010@gamil.com>
2021-09-23 17:24:10 +0900

    Added tabs at the top of screen

--- Udemy/deli_meals/lib/main.dart
+++ Udemy/deli_meals/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
+import 'screens/tabs_screen.dar.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,7 +33,8 @@ class MyApp extends StatelessWidget {
       // home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        // '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
--- Udemy/deli_meals/lib/screens/categories_screen.dart
+++ Udemy/deli_meals/lib/screens/categories_screen.dart
@@ -7,27 +7,29 @@ import '../models/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DeliMeal"),
-        titleTextStyle: Theme.of(context).textTheme.headline6,
+    return
+        // Scaffold(
+        // appBar: AppBar(
+        //   title: const Text("DeliMeal"),
+        //   titleTextStyle: Theme.of(context).textTheme.headline6,
+        // ),
+        // body:
+        GridView(
+      padding: const EdgeInsets.all(25),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children: DUMMY_CATEGORIES
-            .map((category) => CategoryItem(
-                  id: category.id,
-                  title: category.title,
-                  color: category.color,
-                ))
-            .toList(),
-      ),
+      children: DUMMY_CATEGORIES
+          .map((category) => CategoryItem(
+                id: category.id,
+                title: category.title,
+                color: category.color,
+              ))
+          .toList(),
+      // ),
     );
   }
 }
